2a8fb4b5c4c27c8b94ee00ed0c5668741f798ff4
Ryan-Luka <ryanlukao@gmail.com>
2025-06-09 21:58:38 -0300

    melhorando as funções e o menu

--- funcoes_sql.py
+++ funcoes_sql.py
@@ -30,9 +30,9 @@ def listar_manifestacoes_por_tipo(conexao):
                 tipo_escolhido = tipos_manifestacao[opcao]
                 break
             else:
-                print("Tipo inválido. Digite novamente... \n")
+                print("\nTipo inválido. Digite novamente... \n")
         except ValueError:
-            print("Entrada inválida. Digite um número válido.\n")
+            print("\nEntrada inválida. Digite um número válido.\n")
 
     os.system('cls' if os.name == 'nt' else 'clear') #limpa a tela do terminal
 
@@ -53,37 +53,35 @@ def listar_manifestacoes_por_tipo(conexao):
 def criar_manifestacao(conexao): #cria uma nova manifestação
     titulo = input("Digite o título da manifestação: ").strip() #remove espaços em branco
     nome = input("Digite o seu nome: ").strip() #remove espaços em branco
+    tipos_manifestacao = {
+        1: "Reclamação",
+        2: "Sugestão",
+        3: "Elogio",
+        4: "Denúncia"
+    }
     while True: #loop para garantir que o tipo da manifestação seja válido
-        tipo = int(input("""
-Escolha o tipo da manifestação:
-1. Reclamação
-2. Sugestão
-3. Elogio
-4. denúncia
-Opcão: """)) #solicita o tipo da manifestação
-        if tipo in (1, 2, 3, 4): #verifica se o tipo da manifestação é válido
-            if tipo == 1:
-                tipo = "Reclamação"
-            elif tipo == 2:
-                tipo = "Sugestão"
-            elif tipo == 3:
-                tipo = "Elogio"
-            elif tipo == 4:
-                tipo = "Denúncia"
-            break #sai do loop se o tipo for válido
-        else:
-            print("\nTipo inválido. Digite novamente.")
+        try:
+            print("Escolha o tipo da manifestação:")
+            for chave, valor in tipos_manifestacao.items():
+                print(f"{chave}. {valor}")
+            opcao = int(input("Opção: "))
+            if opcao in tipos_manifestacao:
+                tipo_escolhido = tipos_manifestacao[opcao]
+                break
+            else:
+                print("\nTipo inválido. Digite novamente... \n")
+        except ValueError:
+            print("\nEntrada inválida. Digite um número válido.\n")
 
     while True: #loop para garantir que a descrição da manifestação seja válida
         descricao = input("\nDigite a mensagem da manifestação: ").strip() #remove espaços em branco
         if len(descricao) <= 3: #verifica se a descrição é maior que 3 caracteres
             print("\nA descrição deve ter mais de 3 caracteres. Digite novamente.\n")
-            input("\nPressione Enter para continuar...") #aguarda o usuário pressionar Enter para continuar
         else:
             break #sai do loop se a descrição for válida
 
     consulta = "insert into manifestacoes (titulo, tipo, autor, descricao) values (%s, %s, %s, %s)" #consulta para inserir os dados da manifestação
-    dados = (titulo, tipo, nome, descricao) #cria uma tupla com os dados da manifestação
+    dados = (titulo, tipo_escolhido, nome, descricao) #cria uma tupla com os dados da manifestação
     insertNoBancoDados(conexao, consulta, dados) #chama a função insertNoBancoDados do arquivo operacoesbd.py
     print("\nManifestação cadastrada com sucesso!") #imprime mensagem de sucesso
 
@@ -99,15 +97,21 @@ def pesquisar_manifestacao(conexao):
     if len(manifestacoes) == 0: #verifica se a lista está vazia
         print("Nenhuma manifestação cadastrada.")
     else:
-        codigo = int(input("Digite o código da manifestação que deseja pesquisar: "))
-        print() #imprime uma linha em branco para melhor visualização
-        consulta = "select * from manifestacoes where codigo = %s"
-        dados = [codigo]
-        manifestacao = listarBancoDados(conexao, consulta, dados)
-        if len(manifestacao) == 0:
-            print("Manifestação não encontrada.")
-        else:
-            print(f"Código: {manifestacao[0][0]}\nTítulo: {manifestacao[0][1]}\nTipo: {manifestacao[0][2]}\nAutor: {manifestacao[0][3]}\nDescrição: {manifestacao[0][4]}\n")
+        while True:
+            try:
+                codigo = int(input("Digite o código da manifestação que deseja pesquisar: "))
+                print() #imprime uma linha em branco para melhor visualização
+                consulta = "select * from manifestacoes where codigo = %s"
+                dados = [codigo]
+                manifestacao = listarBancoDados(conexao, consulta, dados)
+                if len(manifestacao) == 0:
+                    print("Manifestação não encontrada.")
+                    break
+                else:
+                    print(f"Código: {manifestacao[0][0]}\nTítulo: {manifestacao[0][1]}\nTipo: {manifestacao[0][2]}\nAutor: {manifestacao[0][3]}\nDescrição: {manifestacao[0][4]}\n")
+                    break
+            except ValueError:
+                print("\nEntrada inválida. Digite um número válido.\n")
     
 def excluir_manifestacao(conexao):
     consulta = "select * from manifestacoes"
@@ -116,15 +120,21 @@ def excluir_manifestacao(conexao):
     if len(manifestacoes) == 0: #verifica se a lista está vazia
         print("Nenhuma manifestação cadastrada.")
     else:
-        codigo = int(input("Digite o código da manifestação que deseja excluir: "))
-        print() #imprime uma linha em branco para melhor visualização
-        consulta = "delete from manifestacoes where codigo = %s"
-        dados = [codigo]
-        linhas_afetadas = excluirBancoDados(conexao, consulta, dados)
-        if linhas_afetadas == 0:
-            print("Manifestação não encontrada.")
-        else:
-            print(f"Manifestação com código {codigo} excluída com sucesso.")
+        while True:
+            try:
+                codigo = int(input("Digite o código da manifestação que deseja excluir: "))
+                print() #imprime uma linha em branco para melhor visualização
+                consulta = "delete from manifestacoes where codigo = %s"
+                dados = [codigo]
+                linhas_afetadas = excluirBancoDados(conexao, consulta, dados)
+                if linhas_afetadas == 0:
+                    print("Manifestação não encontrada.")
+                    break
+                else:
+                    print(f"Manifestação com código {codigo} excluída com sucesso.")
+                    break
+            except ValueError:
+                print("\nEntrada inválida. Digite um número válido.\n")
 
     
 
